adf904338196be2252a579b3d5e9bbaf88cda8b4
Darcy Clarke <darcy@darcyclarke.me>
2021-03-26 02:38:01 -0400

    fix: try/catch

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 module.exports = iss = async () => {
   const fetch = require('node-fetch')
   const cheerio = require('cheerio')
-  const response = await fetch('https://istheshipstillstuck.com/')
-  const $ = cheerio.load(await response.text())
-  const status = trim($('h1+p a').text().toLowerCase().replace('.',''))
-  return status === 'yes'
+  try {
+    const response = await fetch('https://istheshipstillstuck.com/')
+    const $ = cheerio.load(await response.text())
+    const status = trim($('h1+p a').text().toLowerCase().replace('.',''))
+    return status === 'yes'
+  } catch (e) {
+    return true
+  }
 }
